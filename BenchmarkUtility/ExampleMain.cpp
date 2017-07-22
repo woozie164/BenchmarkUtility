69c97529660f62012c3d126f3de262d5efb93c38
woozie164 <woozie663@gmail.com>
2017-07-22 17:36:42 +0200

    cleanup

--- BenchmarkUtility/ExampleMain.cpp
+++ BenchmarkUtility/ExampleMain.cpp
@@ -8,52 +8,12 @@ float foo( int x, float y )
 	return x + y;
 }
 
-void V1(int iterations)
-{	
-	vector<float> cTimes( iterations );
-	vector<float> cTimeStart( iterations );
-	vector<float> cTimeEnd( iterations );
-
-	vector<float> wTimes( iterations );
-	vector<float> wTimeStart( iterations );
-	vector<float> wTimeEnd( iterations );
-
-	for (int i = 0; i < iterations; i++)
-	{
-		cTimes[i] = ((float)(cTimeEnd[i] - cTimeStart[i])) / CLOCKS_PER_SEC;
-		wTimes[i] = duration<float>( wTimeEnd[i] - wTimeStart[i] ).count();
-	}
-
-}
-
-void V2(int iterations)
-{
-	vector<float> cTimes( iterations );
-	vector<float> cTimeStart( iterations );
-	vector<float> cTimeEnd( iterations );
-
-	for (int i = 0; i < iterations; i++)
-	{
-		cTimes[i] = ((float)(cTimeEnd[i] - cTimeStart[i])) / CLOCKS_PER_SEC;		
-	}
-
-	vector<float> wTimes( iterations );
-	vector<float> wTimeStart( iterations );
-	vector<float> wTimeEnd( iterations );
-
-	for (int i = 0; i < iterations; i++)
-	{
-		wTimes[i] = duration<float>( wTimeEnd[i] - wTimeStart[i] ).count();
-	}
-
-}
-
 void main()
 {
 	auto f = std::bind( foo, 1, 2.0f );
 
 	BenchmarkResult result = benchmark( f );	
-	BenchmarkResult result2 = benchmark( f, 10000 );
+	BenchmarkResult result2 = benchmark( f, 10'000 );
 
 	int iterations = 1'000'000;
 	BenchmarkResult result3 = benchmark( std::bind( V1, iterations ) );
